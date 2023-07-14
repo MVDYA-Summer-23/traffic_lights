dc0d97db7580d8d8e0b97ada5f80d31f92738535
Mario Vellandi <mvellandi@hotmail.com>
2023-07-14 22:19:21 +0200

    update grid test and transition implementation

--- lib/traffic_lights/grid.ex
+++ lib/traffic_lights/grid.ex
@@ -9,25 +9,33 @@ defmodule TrafficLights.Grid do
   """
   @doc "Start the grid with a list of five TrafficLights.Light pids"
   def start_link(_opts \\ []) do
-    lights = Enum.map(1..5, fn _ -> Light.start_link() |> elem(1) end)
-    GenServer.start_link(__MODULE__, lights)
+    lights =
+      Enum.reduce(1..5, %{}, fn idx, grid ->
+        {:ok, pid} = Light.start_link()
+        Map.put(grid, idx, pid)
+      end)
+
+    GenServer.start_link(__MODULE__, %{lights: lights, current_light: 1})
   end
 
   @doc "Retrieve the current state of all light in the grid"
   def current_lights(pid), do: GenServer.call(pid, :current_lights)
 
-  @doc "Transition all lights in the grid"
+  @doc "Transition one light in the grid, in a sequence"
   def transition(pid), do: GenServer.cast(pid, :transition)
 
   def init(data), do: {:ok, data}
 
-  def handle_call(:current_lights, _from, state) do
-    lights = Enum.map(state, fn light -> Light.current_light(light) end)
-    {:reply, lights, state}
+  def handle_call(:current_lights, _from, %{lights: lights} = state) do
+    lights_status = Enum.map(lights, fn {_idx, light} -> Light.current_light(light) end)
+    {:reply, lights_status, state}
   end
 
-  def handle_cast(:transition, state) do
-    Enum.each(state, fn light -> Light.transition(light) end)
-    {:noreply, state}
+  def handle_cast(:transition, %{lights: lights, current_light: current_index} = state) do
+    max_lights = Map.keys(lights) |> length()
+    current_light_pid = Map.get(lights, current_index)
+    Light.transition(current_light_pid)
+    next_light = if current_index == max_lights, do: 1, else: current_index + 1
+    {:noreply, %{state | current_light: next_light}}
   end
 end
--- test/traffic_lights/grid_test.exs
+++ test/traffic_lights/grid_test.exs
@@ -3,33 +3,23 @@ defmodule TrafficLights.GridTest do
   use ExUnit.Case
   doctest Grid
 
-  setup_all [:start_grid]
+  test "start_link starts a Grid GenServer" do
+    assert {:ok, pid} = Grid.start_link()
+  end
 
-  defp start_grid(_context) do
+  test "current_lights/1 returns a list of 5 Light phases, each initialized to green" do
     {:ok, pid} = Grid.start_link()
-    [pid: pid]
+    assert [:green, :green, :green, :green, :green] = Grid.current_lights(pid)
   end
 
-  @tag :skip_setup
-  test "start_link starts a Grid GenServer with 5 Light pids" do
+  test "transition/1 transitions all lights in the grid to the next phase" do
     {:ok, pid} = Grid.start_link()
-    grid = :sys.get_state(pid)
-    Enum.each(grid, fn light -> assert is_pid(light) end)
-  end
-
-  test "current_lights/1 returns a list of 5 Light processes, each initialized to green",
-       context do
-    [:green, :green, :green, :green, :green] = Grid.current_lights(context[:pid])
-  end
-
-  test "transition/1 transitions all lights in the grid to the next phase",
-       context do
-    [:green, :green, :green, :green, :green] = Grid.current_lights(context[:pid])
-    Grid.transition(context[:pid])
-    [:yellow, :yellow, :yellow, :yellow, :yellow] = Grid.current_lights(context[:pid])
-    Grid.transition(context[:pid])
-    [:red, :red, :red, :red, :red] = Grid.current_lights(context[:pid])
-    Grid.transition(context[:pid])
-    [:green, :green, :green, :green, :green] = Grid.current_lights(context[:pid])
+    [:green, :green, :green, :green, :green] = Grid.current_lights(pid)
+    Grid.transition(pid)
+    [:yellow, :green, :green, :green, :green] = Grid.current_lights(pid)
+    Grid.transition(pid)
+    [:yellow, :yellow, :green, :green, :green] = Grid.current_lights(pid)
+    Grid.transition(pid)
+    [:yellow, :yellow, :yellow, :green, :green] = Grid.current_lights(pid)
   end
 end
